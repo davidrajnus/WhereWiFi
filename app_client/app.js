c4f03192b240cffcbd8422afca100703a97f5344
DRAJ <davidrajnus@gmail.com>
2017-11-03 00:41:37 +0800

    added login page

--- app_client/app.js
+++ app_client/app.js
@@ -1,26 +1,30 @@
 (function () {
 
-angular.module('wherewifiApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
+angular
+  .module('wherewifiApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
 
 function config ($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'home/home.view.html',
+      templateUrl: '/home/home.view.html',
       controller: 'homeCtrl',
       controllerAs: 'vm'
     })
     .when('/about', {
-      templateUrl: 'common/views/genericText.view.html',
+      templateUrl: '/common/views/genericText.view.html',
       controller: 'aboutCtrl',
       controllerAs: 'vm'
     })
     .when('/location/:locationid', {
-      templateUrl:'/locationDetail/locationDetail.view.html',
+      templateUrl: '/locationDetail/locationDetail.view.html',
       controller: 'locationDetailCtrl',
       controllerAs: 'vm'
     })
-    .otherwise({redirectTo: '/'});
-
+    .when('/register', {
+      templateUrl: '/auth/register/register.view.html',
+      controller: 'registerCtrl',
+      controllerAs: 'vm'
+    })
   $locationProvider.html5Mode(true);
 }
 
--- app_client/common/services/authentication.service.js
+++ app_client/common/services/authentication.service.js
@@ -3,63 +3,66 @@
     .module('wherewifiApp')
     .service('authentication', authentication);
 
-  authentication.$inject = ['$window'];
-  function authentication ($window) {
+  authentication.$inject = ['$http', '$window'];
+    function authentication ($http, $window) {
 
-    var saveToken = function (token) {
-      $window.localStorage['wherewifi-token'] = token;
-    };
+      var saveToken = function (token) {
+        $window.localStorage['wherewifi-token'] = token;
+      };
 
-    var getToken = function (token) {
-      return $window.localStorage['wherewifi-token'];
-    };
+      var getToken = function () {
+        return $window.localStorage['wherewifi-token'];
+      };
 
-    var isLoggedIn = function() {
-      var token = getToken();
-
-      if (token) {
-        var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-        return payload.exp > Date.now()/1000;
-      } else {
-        return false;
-      }
-    };
-
-    var currentUser = function() {
-      if (isLoggedIn()) {
+      var isLoggedIn = function() {
         var token = getToken();
-        var payload = JSON.parse($window.atob(token.split('.')[1]));
-        return {
-          email : payload.email,
-          name : payload.name
+
+        if(token){
+          var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+          return payload.exp > Date.now() / 1000;
+        } else {
+          return false;
         }
-      }
+      };
+
+      var currentUser = function() {
+        if(isLoggedIn()){
+          var token = getToken();
+          var payload = JSON.parse($window.atob(token.split('.')[1]));
+          return {
+            email : payload.email,
+            name : payload.name
+          };
+        }
+      };
+
+      register = function(user) {
+        return $http.post('/api/register', user).success(function(data){
+          saveToken(data.token);
+        });
+      };
+
+      login = function(user) {
+        return $http.post('/api/login', user).success(function(data) {
+          saveToken(data.token);
+        });
+      };
+
+      logout = function() {
+        $window.localStorage.removeItem('wherewifi-token');
+      };
+
+      return {
+        currentUser : currentUser,
+        saveToken : saveToken,
+        getToken : getToken,
+        isLoggedIn : isLoggedIn,
+        register : register,
+        login : login,
+        logout : logout
+      };
     }
 
-    register = function(user) {
-      return $http.post('/api/register', user).success(function(data) {
-        saveToken(data.token);
-      });
-    };
 
-    login = function(user) {
-      return $http.post('/api/login', user).success(function(data) {
-        saveToken(data.token);
-      });
-    };
-
-    logout = function () {
-      $window.localStorage.removeItem('wherewifi-token');
-    };
-
-    return {
-      saveToken : saveToken,
-      getToken : getToken,
-      register : register,
-      login : login,
-      logout :logout,
-      isLoggedIn : isLoggedIn
-    };
-  }
-})();
+  })();
